cc9d3d782eca53721319477835bd170c94e1aff0
gaolihao <justunyeyeye@gmail.com>
2026-07-07 17:00:42 -0400

    add controllers and routers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
-const express = require('express');
+import express from 'express';
+import healthRouter from './routers/healthRouter.js';
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use('/', healthRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
