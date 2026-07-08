9e8d7369906540028a7755c7c9a43ad84a6804f7
gaolihao <justunyeyeye@gmail.com>
2026-07-08 00:18:43 -0400

    small adjust to cors in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,29 @@
 import express from 'express';
 import healthRouter from './routers/healthRouter.js';
 import detectionRouter from './routers/detectionRouter.js';
+import cors from 'cors';
 
 const app = express();
 
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+};
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', corsOptions.origin);
+    res.setHeader('Access-Control-Allow-Methods', corsOptions.methods.join(', '));
+    res.setHeader('Access-Control-Allow-Headers', corsOptions.allowedHeaders.join(', '));
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/', healthRouter);
